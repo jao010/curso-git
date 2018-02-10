52442b1df643d0d2da15e3490f99763d6168a1a6
joão paulo <joopaulo157@hotmail.com>
2018-02-10 16:26:22 -0200

    ola mundo

---
+++ git-teste/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git_teste
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Console.WriteLine("Ola mundo!");
+
+			Console.ReadKey();
+
+		}
+	}
+}
